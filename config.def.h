300007e7a7f041d9416d89f009d4f6c32dcd6bbe
Linux User <bitheaven@localhost.localdomain>
2022-08-19 17:51:32 +0000

    hz

--- config.def.h
+++ config.def.h
@@ -37,9 +37,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class       instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",      NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",   NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "xwinwrap",  NULL,       NULL,       1 << 9,       0,           -1 },
+	{ NULL,        NULL,       "broken",   1 << 9,       0,           -1 },
 };
 
 /* layout(s) */

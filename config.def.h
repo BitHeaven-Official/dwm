e7b210bc365d46515ecd6d0de73e5d11d3c0b391
BitHeaven <33015545+BitHeaven-Official@users.noreply.github.com>
2022-09-28 02:11:06 +0500

    Add toggle on top

--- config.def.h
+++ config.def.h
@@ -115,9 +115,10 @@ static Key keys[] = {
 	{ SUPERKEY,                     XK_period,                focusmon,       {.i = +1 } },
 	{ SUPERKEY|ShiftMask,           XK_comma,                 tagmon,         {.i = -1 } },
 	{ SUPERKEY|ShiftMask,           XK_period,                tagmon,         {.i = +1 } },	
-	{ SUPERKEY,                       XK_minus,		  setgaps,        {.i = -1 } },
-	{ SUPERKEY,                       XK_equal,		  setgaps,        {.i = +1 } },
-	{ SUPERKEY|ShiftMask,             XK_equal, 		  setgaps,        {.i = 0  } },
+	{ SUPERKEY,                     XK_minus,		  setgaps,        {.i = -1 } },
+	{ SUPERKEY,                     XK_equal,		  setgaps,        {.i = +1 } },
+	{ SUPERKEY|ShiftMask,           XK_equal, 		  setgaps,        {.i = 0  } },
+	{ SUPERKEY|ShiftMask,           XK_t,			  togglealwaysontop, {0} },
 	TAGKEYS(                        XK_1,                                     0)
 	TAGKEYS(                        XK_2,                                     1)
 	TAGKEYS(                        XK_3,                                     2)

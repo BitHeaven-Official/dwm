d803aa396323830d30f78f8526dbaa41c8aed657
BitHeaven-Official <o.o@bitheaven.ru>
2023-03-08 09:50:09 +0500

    Add xrdb patch

--- config.h
+++ config.h
@@ -19,16 +19,17 @@ static const char *fonts[]          = {
     "Font Awesome 6 Free Solid:style=Solid:size=12:antialias=true:autohint=true",
     "Font Awesome 6 Brands Regular:style=Regular:size=12:antialias=true:autohint=true"
 };
-static const char dmenufont[]       = "Ubuntu Mono:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static char dmenufont[]          = "Ubuntu Mono:size=12";
+static char normbgcolor[]        = "#222222";
+static char normbordercolor[]    = "#444444";
+static char normfgcolor[]        = "#bbbbbb";
+static char selfgcolor[]         = "#eeeeee";
+static char selbordercolor[]     = "#005577";
+static char selbgcolor[]         = "#005577";
+static char *colors[][3]         = {
+	/*               fg           bg           border   */
+	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+	[SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
 /* tagging */
@@ -96,7 +97,7 @@ static const char *volmute[] = { "pactl", "set-sink-mute", "0", "toggle", NULL }
 
 static const char *backlightup[] = { "s", "set-sink-mute", "0", "toggle", NULL };
 
-static const char *dmenucmd[] = { "dmenu_run", "-g", "15", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-g", "15", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
@@ -137,6 +138,7 @@ static Key keys[] = {
 	{ SUPERKEY|ShiftMask,           XK_equal, 		  setgaps,        {.i = 0  } },
 	{ SUPERKEY|ShiftMask,           XK_t,			  togglealwaysontop, {0} },
 	{ SUPERKEY|ShiftMask,           XK_f,			  togglefullscr,  {0} },
+	{ SUPERKEY,                     XK_F5,			  xrdb,           {.v = NULL } },
 	TAGKEYS(                        XK_1,                                     0)
 	TAGKEYS(                        XK_2,                                     1)
 	TAGKEYS(                        XK_3,                                     2)
